b42173dd91628332e8fd2879cbd6b42535dd495b
Jesse <jesse.v.rhijn@gmail.com>
2018-06-24 21:03:24 +0200

    Get rid of cmp in triangle()

--- src/main.rs
+++ src/main.rs
@@ -30,14 +30,15 @@ fn line(mut x0: u32, mut y0: u32, mut x1: u32, mut y1: u32,
 
 fn triangle(face: &geo::Vec3i, model: &model::Obj, img: &mut image::RgbImage, pix: image::Rgb<u8>) {
     let (imgx, imgy) = img.dimensions();
+    let (imgx, imgy) = (imgx - 1, imgy - 1);
     let face = vec![face.x, face.y, face.z];
     for j in 0..3 {
         let v0 = model.vert(face[j] as usize);
         let v1 = model.vert(face[(j+1) % 3] as usize);
-        let x0 = cmp::min(((v0.x + 1.)*(imgx as f64)/2.) as u32, imgx-1);
-        let y0 = cmp::min(((v0.y + 1.)*(imgy as f64)/2.) as u32, imgy-1);
-        let x1 = cmp::min(((v1.x + 1.)*(imgx as f64)/2.) as u32, imgx-1);
-        let y1 = cmp::min(((v1.y + 1.)*(imgy as f64)/2.) as u32, imgy-1);
+        let x0 = ((v0.x + 1. )*0.5*(imgx as f64)) as u32;
+        let y0 = ((v0.y + 1. )*0.5*(imgy as f64)) as u32;
+        let x1 = ((v1.x + 1.)*0.5*(imgx as f64)) as u32;
+        let y1 = ((v1.y + 1.)*0.5*(imgy as f64)) as u32;
         line(x0, y0, x1, y1, img, pix);
     }
 }
